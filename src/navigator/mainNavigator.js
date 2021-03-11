ac8dfc3dd3d3a786819e9a90731d6cf8e4a16b65
Crowdbotics <team@crowdbotics.com>
2021-03-11 01:15:12 +0000

    add package blueprint-react-native-dashboard-2-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native"
 import SplashScreen from "../features/SplashScreen"
 import SideMenu from "./sideMenu"
 //@BlueprintImportInsertion
+import Dashboard24213713Navigator from '../features/Dashboard24213713/navigator';
 
 /**
  * new navigators can be imported here
@@ -12,6 +13,7 @@ import SideMenu from "./sideMenu"
 
 const AppNavigator = {
   //@BlueprintNavigationInsertion
+Dashboard24213713: { screen: Dashboard24213713Navigator },
 
   /** new navigators can be added here */
   SplashScreen: {
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import Dashboard24213713Reducer from '../features/Dashboard24213713/redux/reducers'
 
 export const combinedReducers = combineReducers({
   blank: (state, action) => {
@@ -15,5 +16,6 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+Dashboard24213713: Dashboard24213713Reducer,
 
 });
